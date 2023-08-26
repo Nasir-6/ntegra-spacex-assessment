ad6a9af6659df79255035f33091ec86f61349c4d
Nasir <nasir-islam@hotmail.com>
2023-08-26 21:08:27 +0100

    create Landings Table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Typography } from '@mui/material';
 import React from 'react';
 import './App.css';
+import LandingsTable from './components/landings_table/LandingsTable';
 
-function App() {
-  return (
-    <div className="App">
-      <Typography variant="h1">SpaceX Landing Page</Typography>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Typography variant="h1">SpaceX Landing Page</Typography>
+    <LandingsTable />
+  </div>
+);
 
 export default App;
---
+++ src/components/landings_table/LandingsTable.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// type Props = {}
+
+const LandingsTable = () => {
+  console.log('Landing table');
+  return <div>LandingsTable</div>;
+};
+
+export default LandingsTable;
