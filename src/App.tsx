d3695662bc42cb181d8ea733f107dcce0f05f1a3
Nasir <nasir-islam@hotmail.com>
2023-08-26 21:01:24 +0100

    install mui and use for h1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">SpaceX Landing Page</div>;
+  return (
+    <div className="App">
+      <Typography variant="h1">SpaceX Landing Page</Typography>
+    </div>
+  );
 }
 
 export default App;
