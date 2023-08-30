3f9eaa7fb6b3944d463e71361b16f03f9e039533
Nasir <nasir-islam@hotmail.com>
2023-08-30 16:25:54 +0100

    remove duplicate test

--- src/components/launch_modal/LaunchModal.test.tsx
+++ src/components/launch_modal/LaunchModal.test.tsx
@@ -36,24 +36,7 @@ describe('LaunchModal rendering', () => {
     expect(screen.getByText(/Engine failure at 33 seconds and loss of vehicle/i)).toBeInTheDocument();
   });
 
-  it('should render the LaunchModal correctly with FalconSat details', async () => {
-    render(<MockedLaunchModal id="5eb87cd9ffd86e000604b32a" />);
-    expect(await screen.findByText(/FalconSat/i)).toBeInTheDocument();
-    expect(screen.getByText(/Failed/i)).toBeInTheDocument();
-    expect(screen.getByText(/Launch Date /i)).toBeInTheDocument();
-    expect(screen.getByText(/2006-03-24/i)).toBeInTheDocument();
-
-    expect(screen.getByText(/Rocket ID:/i)).toBeInTheDocument();
-    expect(screen.getByText(/5e9d0d95eda69955f709d1eb/i)).toBeInTheDocument();
-
-    expect(screen.getByText(/Launchpad ID:/i)).toBeInTheDocument();
-    expect(screen.getByText(/5e9e4502f5090995de566f86/i)).toBeInTheDocument();
-
-    expect(screen.getByText(/Details:/i)).toBeInTheDocument();
-    expect(screen.getByText(/Engine failure at 33 seconds and loss of vehicle/i)).toBeInTheDocument();
-  });
-
-  it('should render the Success chip for RatSat details as launch.success is true', async () => {
+  it('should render the Success chip for RatSat details since launch.success is true', async () => {
     render(<MockedLaunchModal id="5eb87cdbffd86e000604b32d" />);
     expect(await screen.findByText('RatSat')).toBeInTheDocument();
     expect(screen.getByText('Success')).toBeInTheDocument();
@@ -66,7 +49,7 @@ describe('LaunchModal rendering', () => {
     expect(screen.queryByText('Failure')).not.toBeInTheDocument();
   });
 
-  it('should render the N/A for Transporter-6 details as launch.details is null', async () => {
+  it('should render N/A for Transporter-6 details as launch.details is null', async () => {
     render(<MockedLaunchModal id="633f72580531f07b4fdf59c6" />);
     expect(await screen.findByText('Transporter-6')).toBeInTheDocument();
     expect(screen.getByText('N/A')).toBeInTheDocument();
