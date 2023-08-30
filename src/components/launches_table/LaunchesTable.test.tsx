d53731f62f9e56c2fd06915b3e2be4fd335e5116
Nasir <nasir-islam@hotmail.com>
2023-08-30 16:23:30 +0100

    refactor LaunchesTable tests

--- src/components/launches_table/LaunchesTable.test.tsx
+++ src/components/launches_table/LaunchesTable.test.tsx
@@ -1,6 +1,5 @@
 import React, { PropsWithChildren } from 'react';
 import { render, screen } from '@testing-library/react';
-// import { debug } from 'console';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import userEvent from '@testing-library/user-event';
@@ -15,6 +14,28 @@ const wrapper = ({ children }: PropsWithChildren) => {
   );
 };
 
+const show10RowsPerPage = () => {
+  const rowsPerPageInputBtn = screen.getByRole('button', { name: 'Rows per page: 5' });
+  userEvent.click(rowsPerPageInputBtn);
+  const option10PerPage = screen.getByRole('option', { name: '10' });
+  userEvent.click(option10PerPage);
+};
+
+const expectNumOfLaunchRowsToBe = (numOfRows: number) => {
+  const launchRows = screen.getAllByTestId(/launch-row-/i);
+  expect(launchRows.length).toBe(numOfRows);
+};
+
+const clickNextPageBtn = () => {
+  const nextPageBtn = screen.getByRole('button', { name: 'Go to next page' });
+  userEvent.click(nextPageBtn);
+};
+
+const clickPrevPageBtn = () => {
+  const prevPageBtn = screen.getByRole('button', { name: 'Go to previous page' });
+  userEvent.click(prevPageBtn);
+};
+
 describe('LaunchTable rendering', () => {
   it('should render the Launch Table correctly', async () => {
     render(<LaunchesTable />, { wrapper });
@@ -22,9 +43,8 @@ describe('LaunchTable rendering', () => {
 
     const headerRow = screen.getByTestId(`header-row`);
     expect(headerRow.childElementCount).toBe(4);
-    const launchRows = screen.getAllByTestId(/launch-row-/i);
-    expect(launchRows.length).toBe(5);
-    expect(screen.getByText(/Rows per page:/i)).toBeInTheDocument();
+    expectNumOfLaunchRowsToBe(5);
+    expect(screen.getByTestId('pagination')).toBeInTheDocument();
   });
 });
 
@@ -32,35 +52,26 @@ describe('LaunchTable pagination', () => {
   it('should display launches 6-10 when clicking on next page button', async () => {
     render(<LaunchesTable />, { wrapper });
     expect(await screen.findByRole('table')).toBeInTheDocument();
-    const nextPageBtn = screen.getByRole('button', { name: 'Go to next page' });
-    userEvent.click(nextPageBtn);
-
+    clickNextPageBtn();
     // No NEED TO USE wait for elementToBeRemoved or act - https://github.com/testing-library/user-event/discussions/906 - use override in package.json to use same @testing-library/dom version!
-    expect(screen.getByText(/CRS-26/i)).toBeInTheDocument();
+    expect(screen.getByText(/CRS-26/i)).toBeInTheDocument(); // 6th row
     expect(screen.queryByText(/FalconSat/i)).toBeNull();
-    const launchRows = screen.getAllByTestId(/launch-row-/i);
-    expect(launchRows.length).toBe(5);
-    // screen.debug();
-    // const pagination = screen.getByTestId('pagination');
-    // screen.debug(pagination);
+    expectNumOfLaunchRowsToBe(5);
   });
 
   it('should display launch 11 with empty rows when clicking on next page button twice', async () => {
     render(<LaunchesTable />, { wrapper });
     expect(await screen.findByRole('table')).toBeInTheDocument();
 
-    const nextPageBtn = screen.getByRole('button', { name: 'Go to next page' });
-    userEvent.click(nextPageBtn);
-
+    clickNextPageBtn();
     expect(screen.getByText(/CRS-26/i)).toBeInTheDocument();
     expect(screen.queryByText(/FalconSat/i)).toBeNull();
 
-    userEvent.click(nextPageBtn);
-    expect(await screen.findByText(/Transporter-6/i)).toBeInTheDocument();
+    clickNextPageBtn();
+    expect(screen.getByText(/Transporter-6/i)).toBeInTheDocument(); // 11th Row
     expect(screen.queryByText(/FalconSat/i)).toBeNull();
     expect(screen.queryByText(/CRS-26/i)).toBeNull();
-    const launchRows = screen.getAllByTestId(/launch-row-/i);
-    expect(launchRows.length).toBe(1);
+    expectNumOfLaunchRowsToBe(1);
     expect(screen.getByTestId(/empty-rows/i)).toBeInTheDocument();
   });
 
@@ -68,90 +79,62 @@ describe('LaunchTable pagination', () => {
     render(<LaunchesTable />, { wrapper });
     expect(await screen.findByRole('table')).toBeInTheDocument();
 
-    const nextPageBtn = screen.getByRole('button', { name: 'Go to next page' });
-    userEvent.click(nextPageBtn);
+    clickNextPageBtn();
     expect(screen.getByText(/CRS-26/i)).toBeInTheDocument();
     expect(screen.queryByText(/FalconSat/i)).toBeNull();
 
-    const prevPageBtn = screen.getByRole('button', { name: 'Go to previous page' });
-    userEvent.click(prevPageBtn);
+    clickPrevPageBtn();
     expect(screen.getByText(/FalconSat/i)).toBeInTheDocument();
     expect(screen.queryByText(/CRS-26/i)).toBeNull();
-
-    const launchRows = screen.getAllByTestId(/launch-row-/i);
-    expect(launchRows.length).toBe(5);
+    expectNumOfLaunchRowsToBe(5);
   });
 
   it('should display launches 1-10 when changing rows per page to 10', async () => {
     render(<LaunchesTable />, { wrapper });
     expect(await screen.findByRole('table')).toBeInTheDocument();
-
     expect(screen.getByText(/FalconSat/i)).toBeInTheDocument();
     expect(screen.queryByText(/CRS-26/i)).toBeNull();
 
-    // userEvent.click(screen.getByRole('button', { name: 'Rows per page: 5' }));
-    const rowsPerPageInputBtn = screen.getByRole('button', { name: 'Rows per page: 5' });
-    userEvent.click(rowsPerPageInputBtn);
-    const option10PerPage = screen.getByRole('option', { name: '10' });
-    userEvent.click(option10PerPage);
-
-    expect(await screen.findByText(/CRS-26/i)).toBeInTheDocument();
+    show10RowsPerPage();
+    expect(screen.getByText(/CRS-26/i)).toBeInTheDocument();
     expect(screen.getByText(/FalconSat/i)).toBeInTheDocument();
-
-    const launchRows = screen.getAllByTestId(/launch-row-/i);
-    expect(launchRows.length).toBe(10);
+    expectNumOfLaunchRowsToBe(10);
   });
 
-  it('should display launches 11 with empty rows when changing rows per page to 10 and clicking next page', async () => {
+  it('should display launch 11 with empty rows when changing rows per page to 10 and clicking next page', async () => {
     render(<LaunchesTable />, { wrapper });
     expect(await screen.findByRole('table')).toBeInTheDocument();
-
     expect(screen.getByText(/FalconSat/i)).toBeInTheDocument();
 
-    const rowsPerPageInputBtn = screen.getByRole('button', { name: 'Rows per page: 5' });
-    userEvent.click(rowsPerPageInputBtn);
-    const option10PerPage = screen.getByRole('option', { name: '10' });
-    userEvent.click(option10PerPage);
+    show10RowsPerPage();
+    expect(screen.getByText(/CRS-26/i)).toBeInTheDocument();
 
-    expect(await screen.findByText(/CRS-26/i)).toBeInTheDocument();
-
-    const nextPageBtn = screen.getByRole('button', { name: 'Go to next page' });
-    userEvent.click(nextPageBtn);
+    clickNextPageBtn();
     expect(screen.queryByText(/CRS-26/i)).toBeNull();
     expect(screen.queryByText(/FalconSat/i)).toBeNull();
-
     expect(screen.getByText(/Transporter-6/i)).toBeInTheDocument();
-    const launchRows = screen.getAllByTestId(/launch-row-/i);
-    expect(launchRows.length).toBe(1);
+    expectNumOfLaunchRowsToBe(1);
     expect(screen.getByTestId(/empty-rows/i)).toBeInTheDocument();
   });
 
   it('should reset to page 1 showing launches 1-10 when changing rows per page to 10 whilst on page 3', async () => {
     render(<LaunchesTable />, { wrapper });
     expect(await screen.findByRole('table')).toBeInTheDocument();
-
     expect(screen.getByText(/FalconSat/i)).toBeInTheDocument();
 
-    const nextPageBtn = screen.getByRole('button', { name: 'Go to next page' });
-    userEvent.click(nextPageBtn);
+    clickNextPageBtn();
     expect(screen.getByText(/CRS-26/i)).toBeInTheDocument();
     expect(screen.queryByText(/FalconSat/i)).toBeNull();
 
-    userEvent.click(nextPageBtn);
+    clickNextPageBtn();
     expect(screen.getByText(/Transporter-6/i)).toBeInTheDocument();
     expect(screen.queryByText(/FalconSat/i)).toBeNull();
     expect(screen.queryByText(/CRS-26/i)).toBeNull();
 
-    const rowsPerPageInputBtn = screen.getByRole('button', { name: 'Rows per page: 5' });
-    userEvent.click(rowsPerPageInputBtn);
-    const option10PerPage = screen.getByRole('option', { name: '10' });
-    userEvent.click(option10PerPage);
-
+    show10RowsPerPage();
     expect(screen.getByText(/CRS-26/i)).toBeInTheDocument();
     expect(screen.getByText(/FalconSat/i)).toBeInTheDocument();
-
     expect(screen.queryByText(/Transporter-6/i)).toBeNull();
-    const launchRows = screen.getAllByTestId(/launch-row-/i);
-    expect(launchRows.length).toBe(10);
+    expectNumOfLaunchRowsToBe(10);
   });
 });
